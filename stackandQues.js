b6fa93435c22eeff1d97dceb2bd2edb0433f840f
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-05-29 10:22:44 +0530

    stack push

---
+++ stackandQues.js
@@ -0,0 +1,32 @@
+class Node{
+    constructor(data){
+        this.data=data;
+        this.next=null
+    }
+}
+class Stack{
+    constructor(){
+        this.top=null;
+    }
+    pushValue=(data)=>{
+        const newNode=new Node(data)
+       if(this.top==null){
+          this.top=newNode;
+          return;
+       }
+       newNode.next=this.top;
+       this.top=newNode;
+    }
+    display(){
+        var temp=this.top;
+        while(temp){
+            console.log(temp.data);
+            temp=temp.next;
+        }
+    }
+}
+const stack=new Stack()
+stack.pushValue(1)
+stack.pushValue(2)
+stack.pushValue(3)
+stack.display()
