9e2680fd6ba480fb7a71663ad2da7859e0b8cd8c
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-06-09 00:47:08 +0530

    code to check the edges connected or not

--- graph.js
+++ graph.js
@@ -22,26 +22,19 @@ class Graph{
             this.map.get(edge).push(vertex);
         }
     }
-    // deleteVertex(vertex){
-    //     if(this.map.has(vertex)){
-    //         for(let [key,value] of this.map){
-    //             let index=value.indexOf(vertex)
-    //             if(index>0){
-    //                 value.splice(index,1)
-    //             }
-    //         }
-    //     }
-    //     this.map.delete(vertex)
-    // }
     deleteVertex(target) {
         this.map.delete(target)
         for (let [vertex, edges] of this.map) {
             this.map.set(vertex, edges.filter(edge => edge !== target))
         }
     }
-    // hasEdge(vertex1, vertex2) {
-    //     return this.map.get(vertex1).includes(vertex2) && this.map.get(vertex2).includes(vertex1);
-    // }
+    hasEdge(vertex1, vertex2) {
+        if (this.map.has(vertex1)) {
+          const edges = this.map.get(vertex1);
+          return edges.includes(vertex2);
+        }
+        return false;
+      }
       
       
     display() {
@@ -57,11 +50,11 @@ graph.insert("B",2)
 graph.insert("c",1,true)
 graph.display()
 console.log("__________________")
-graph.deleteVertex(1)
-graph.display()
+// graph.deleteVertex(1)
+// graph.display()
 
 // graph.removeEdge("A",1)
 // graph.display()
 // graph.deleteVertex(1)
-// const res=graph.hasEdge("A",1)
-// console.log(res)
+const res=graph.hasEdge(1,"2")
+console.log(res)
