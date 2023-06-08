ede7873a1bc4da97ea9c59308af4a6452378c207
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-06-08 19:48:30 +0530

    trie suffix

--- trie.js
+++ trie.js
@@ -1,3 +1,61 @@
+// class TrieNode {
+//     constructor() {
+//       this.children = new Map();
+//       this.isEndOfWord = false;
+//     }
+//   }
+  
+//   class Trie {
+//     constructor() {
+//       this.root = new TrieNode();
+//     }
+  
+//     insert(word) {
+//       let current = this.root;
+//       for (let i = 0; i < word.length; i++) {
+//         const char = word[i];
+//         if (!current.children.has(char)) {
+//           current.children.set(char, new TrieNode());
+//         }
+//         current = current.children.get(char);
+//       }
+//       current.isEndOfWord = true;
+//     }
+  
+//     search(word) {
+//       let current = this.root;
+//       for (let i = 0; i < word.length; i++) {
+//         const char = word[i];
+//         if (!current.children.has(char)) {
+//           return false;
+//         }
+//         current = current.children.get(char);
+//       }
+//       return current.isEndOfWord;
+//     }
+  
+//     startsWith(prefix) {
+//       let current = this.root;
+//       for (let i = 0; i < prefix.length; i++) {
+//         const char = prefix[i];
+//         if (!current.children.has(char)) {
+//           return false;
+//         }
+//         current = current.children.get(char);
+//       }
+//       return true;
+//     }
+//   }
+// const trie = new Trie();
+// trie.insert("apple");
+// trie.insert("banana");
+// trie.insert("orange");
+// const res=trie.search("orange")
+// console.log(res)
+
+  
+//suffix-------------------------------------------------------->
+
 class TrieNode {
     constructor() {
       this.children = new Map();
@@ -9,8 +67,13 @@ class TrieNode {
     constructor() {
       this.root = new TrieNode();
     }
-  
-    insert(word) {
+    insert(word){
+        for(var i=0;i<word.length;i++){
+            var suffix=word.slice(i)
+            this.insertWord(suffix)
+        }
+    }
+    insertWord(word) {
       let current = this.root;
       for (let i = 0; i < word.length; i++) {
         const char = word[i];
@@ -50,7 +113,5 @@ const trie = new Trie();
 trie.insert("apple");
 trie.insert("banana");
 trie.insert("orange");
-const res=trie.search("orange")
+const res=trie.search("nge")
 console.log(res)
-
-  
